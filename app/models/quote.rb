56a3410e43599ff25b9d77418a048e5939c8eefb
Alexander Dischberg <noxasch@gmail.com>
2023-12-06 12:33:44 +0800

    feat: turbo stream asyncronous hook

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -4,8 +4,8 @@ class Quote < ApplicationRecord
   scope :ordered, -> { order(id: :desc) }
 
   # broadcast_ is syncronous by default
-  after_create_commit -> { broadcast_prepend_to 'quotes' }
-  after_update_commit -> { broadcast_replace_to 'quotes' }
+  after_create_commit -> { broadcast_prepend_later_to 'quotes' }
+  after_update_commit -> { broadcast_replace_later_to 'quotes' }
   after_destroy_commit -> { broadcast_remove_to 'quotes' }
 
   # def update_quotes_list
