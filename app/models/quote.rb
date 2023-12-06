28a5234021fd759b03ef8355bc727c20d8e468de
Alexander Dischberg <noxasch@gmail.com>
2023-12-06 12:32:38 +0800

    feat: turbo stream real time

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -2,4 +2,15 @@ class Quote < ApplicationRecord
   validates :name, presence: true
 
   scope :ordered, -> { order(id: :desc) }
+
+  after_commit :update_quotes_list
+
+  private
+
+  def update_quotes_list
+    broadcast_prepend_to 'quotes',
+                         partial: 'quotes/quote',
+                         locals: { quote: self },
+                         target: 'quotes'
+  end
 end
