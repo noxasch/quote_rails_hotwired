45a5fc66553d9372399aa365db4f716172a11cec
Alexander Dischberg <noxasch@gmail.com>
2023-12-20 23:27:52 +0800

    feat: add company model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_22_080416) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_20_152458) do
+  create_table "companies", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "quotes", force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", null: false
