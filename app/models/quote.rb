64f2e98bb0169981477d5049cd5ba2b49bcd5421
Alexander Dischberg <noxasch@gmail.com>
2023-12-20 23:20:04 +0800

    chore: update notes and deps

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -4,6 +4,8 @@ class Quote < ApplicationRecord
   scope :ordered, -> { order(id: :desc) }
 
   # broadcast_ is syncronous by default
+
+  # ACTION_to is asyncronous
   # after_create_commit -> { broadcast_prepend_later_to 'quotes' }
   # after_update_commit -> { broadcast_replace_later_to 'quotes' }
   # after_destroy_commit -> { broadcast_remove_to 'quotes' }
