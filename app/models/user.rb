761cfb9ec5d128637ccb439a81079c514d4988fc
Alexander Dischberg <noxasch@gmail.com>
2023-12-22 13:39:57 +0800

    fix: fix devise user feature  and home page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  # devise :database_authenticatable, :registerable,
+  #        :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :validatable
 
   belongs_to :company
 
